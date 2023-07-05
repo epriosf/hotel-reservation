98b37062f18703a0f31c0a725ab95b7d159af077
Erick Rios <paulrios@outlook.es>
2023-07-05 16:22:56 -0500

    Initialize the repository with basic configuration

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HomeView from "@/views/HomeView.vue";
+</script>
+
+<template>
+  <body>
+    <HomeView />
+  </body>
+
+</template>
+
+<style lang="scss">
+@import './assets/main.scss';
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+   <h1>Hotel Reservation System </h1>
+</template>
+<style lang="scss">
+@import "./../assets/main.scss";
+</style>
